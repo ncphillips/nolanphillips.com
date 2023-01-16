aff77b16ef277567faa7c0f62424e727e5fca82f
Nolan Phillips <git@nolanphillips.com>
2023-01-16 07:53:12 -0400

    Header always has the site title

--- src/layouts/Sidebar/Header.tsx
+++ src/layouts/Sidebar/Header.tsx
@@ -9,17 +9,19 @@ export type HeaderProps = PropsWithChildren & {
 
 export const Header: React.FC<HeaderProps> = ({ title, backHref, children }) => {
     return (
-      <header className="flex lg:hidden flex-row justify-between gap-2 basis-full shrink-0 h-12 relative z-30 sticky top-0 bg-zinc-100 bg-opacity-70 dark:bg-opacity-70 backdrop-blur-sm dark:bg-zinc-900 border-b border-1 border-zinc-200 dark:border-zinc-800 p-5">
+      <header className="flex width:100% lg:hidden flex-row items-center gap-1 basis-full shrink-0 h-12 relative z-30 sticky top-0 bg-zinc-100 bg-opacity-70 dark:bg-opacity-70 backdrop-blur-sm dark:bg-zinc-900 border-b border-1 border-zinc-200 dark:border-zinc-800 p-5">
         {backHref && (
-          <div className="-ml-3 -mt-3">
+          <div className="-ml-3">
             <ButtonBack defaultHref={backHref} />
           </div>
         )}
-        {!backHref && (
-          <Heading as='h2' size={4}>
-            {title}
+
+        <div>
+          <Heading as='h2' size={2}>
+            Nolan Phillips
           </Heading>
-        )}
+        </div>
+        
         {children}
       </header>
     )
