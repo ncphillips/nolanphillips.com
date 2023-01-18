c5a2f365d19820b8f93afd1db87e16a1548c31ed
Nolan Phillips <nolan@ncphi.com>
2023-01-18 16:12:19 -0400

    Fiddlign with sidebar CSS

--- .astro/types.d.ts
+++ .astro/types.d.ts
@@ -1,102 +1,99 @@
-declare module "astro:content" {
-  export { z } from "astro/zod"
-  export type CollectionEntry<C extends keyof typeof entryMap> =
-    (typeof entryMap)[C][keyof (typeof entryMap)[C]] & Render
+declare module 'astro:content' {
+	export { z } from 'astro/zod';
+	export type CollectionEntry<C extends keyof typeof entryMap> =
+		(typeof entryMap)[C][keyof (typeof entryMap)[C]] & Render;
 
-  type BaseSchemaWithoutEffects =
-    | import("astro/zod").AnyZodObject
-    | import("astro/zod").ZodUnion<import("astro/zod").AnyZodObject[]>
-    | import("astro/zod").ZodDiscriminatedUnion<
-        string,
-        import("astro/zod").AnyZodObject[]
-      >
-    | import("astro/zod").ZodIntersection<
-        import("astro/zod").AnyZodObject,
-        import("astro/zod").AnyZodObject
-      >
+	type BaseSchemaWithoutEffects =
+		| import('astro/zod').AnyZodObject
+		| import('astro/zod').ZodUnion<import('astro/zod').AnyZodObject[]>
+		| import('astro/zod').ZodDiscriminatedUnion<string, import('astro/zod').AnyZodObject[]>
+		| import('astro/zod').ZodIntersection<
+				import('astro/zod').AnyZodObject,
+				import('astro/zod').AnyZodObject
+		  >;
 
-  type BaseSchema =
-    | BaseSchemaWithoutEffects
-    | import("astro/zod").ZodEffects<BaseSchemaWithoutEffects>
+	type BaseSchema =
+		| BaseSchemaWithoutEffects
+		| import('astro/zod').ZodEffects<BaseSchemaWithoutEffects>;
 
-  type BaseCollectionConfig<S extends BaseSchema> = {
-    schema?: S
-    slug?: (entry: {
-      id: CollectionEntry<keyof typeof entryMap>["id"]
-      defaultSlug: string
-      collection: string
-      body: string
-      data: import("astro/zod").infer<S>
-    }) => string | Promise<string>
-  }
-  export function defineCollection<S extends BaseSchema>(
-    input: BaseCollectionConfig<S>
-  ): BaseCollectionConfig<S>
+	type BaseCollectionConfig<S extends BaseSchema> = {
+		schema?: S;
+		slug?: (entry: {
+			id: CollectionEntry<keyof typeof entryMap>['id'];
+			defaultSlug: string;
+			collection: string;
+			body: string;
+			data: import('astro/zod').infer<S>;
+		}) => string | Promise<string>;
+	};
+	export function defineCollection<S extends BaseSchema>(
+		input: BaseCollectionConfig<S>
+	): BaseCollectionConfig<S>;
 
-  export function getEntry<
-    C extends keyof typeof entryMap,
-    E extends keyof (typeof entryMap)[C]
-  >(collection: C, entryKey: E): Promise<(typeof entryMap)[C][E] & Render>
-  export function getCollection<
-    C extends keyof typeof entryMap,
-    E extends keyof (typeof entryMap)[C]
-  >(
-    collection: C,
-    filter?: (data: (typeof entryMap)[C][E]) => boolean
-  ): Promise<((typeof entryMap)[C][E] & Render)[]>
+	export function getEntry<C extends keyof typeof entryMap, E extends keyof (typeof entryMap)[C]>(
+		collection: C,
+		entryKey: E
+	): Promise<(typeof entryMap)[C][E] & Render>;
+	export function getCollection<
+		C extends keyof typeof entryMap,
+		E extends keyof (typeof entryMap)[C]
+	>(
+		collection: C,
+		filter?: (data: (typeof entryMap)[C][E]) => boolean
+	): Promise<((typeof entryMap)[C][E] & Render)[]>;
 
-  type InferEntrySchema<C extends keyof typeof entryMap> =
-    import("astro/zod").infer<
-      Required<ContentConfig["collections"][C]>["schema"]
-    >
+	type InferEntrySchema<C extends keyof typeof entryMap> = import('astro/zod').infer<
+		Required<ContentConfig['collections'][C]>['schema']
+	>;
 
-  type Render = {
-    render(): Promise<{
-      Content: import("astro").MarkdownInstance<{}>["Content"]
-      headings: import("astro").MarkdownHeading[]
-      remarkPluginFrontmatter: Record<string, any>
-    }>
-  }
+	type Render = {
+		render(): Promise<{
+			Content: import('astro').MarkdownInstance<{}>['Content'];
+			headings: import('astro').MarkdownHeading[];
+			remarkPluginFrontmatter: Record<string, any>;
+		}>;
+	};
 
-  const entryMap: {
-    articles: {
-      "design-grammar.mdx": {
-        id: "design-grammar.mdx"
-        slug: "design-grammar"
-        body: string
-        collection: "articles"
-        data: any
-      }
-      "mocking-action-mailer.mdx": {
-        id: "mocking-action-mailer.mdx"
-        slug: "mocking-action-mailer"
-        body: string
-        collection: "articles"
-        data: any
-      }
-      "rails-contract-tests.mdx": {
-        id: "rails-contract-tests.mdx"
-        slug: "rails-contract-tests"
-        body: string
-        collection: "articles"
-        data: any
-      }
-      "testing-for-change.mdx": {
-        id: "testing-for-change.mdx"
-        slug: "testing-for-change"
-        body: string
-        collection: "articles"
-        data: any
-      }
-      "testing-graphql-in-rails.mdx": {
-        id: "testing-graphql-in-rails.mdx"
-        slug: "testing-graphql-in-rails"
-        body: string
-        collection: "articles"
-        data: any
-      }
-    }
-  }
+	const entryMap: {
+		"articles": {
+"design-grammar.mdx": {
+  id: "design-grammar.mdx",
+  slug: "design-grammar",
+  body: string,
+  collection: "articles",
+  data: any
+},
+"mocking-action-mailer.mdx": {
+  id: "mocking-action-mailer.mdx",
+  slug: "mocking-action-mailer",
+  body: string,
+  collection: "articles",
+  data: any
+},
+"rails-contract-tests.mdx": {
+  id: "rails-contract-tests.mdx",
+  slug: "rails-contract-tests",
+  body: string,
+  collection: "articles",
+  data: any
+},
+"testing-for-change.mdx": {
+  id: "testing-for-change.mdx",
+  slug: "testing-for-change",
+  body: string,
+  collection: "articles",
+  data: any
+},
+"testing-graphql-in-rails.mdx": {
+  id: "testing-graphql-in-rails.mdx",
+  slug: "testing-graphql-in-rails",
+  body: string,
+  collection: "articles",
+  data: any
+},
+},
 
-  type ContentConfig = never
+	};
+
+	type ContentConfig = never;
 }
--- src/layouts/Sidebar/Menu.tsx
+++ src/layouts/Sidebar/Menu.tsx
@@ -1,30 +1,37 @@
 import Icon from "../../components/Icon"
 import Text from "../../components/typography/Text"
-import { MenuItem as MenuItemProps, menuItems } from "../../content/_menu"
+import {
+  MenuCategory,
+  MenuItem as MenuItemProps,
+  menuItems,
+} from "../../content/_menu"
+import { cn } from "../../lib/cn"
 
 export const Menu = () => (
-  <ul className="mt-4 flex flex-col gap-2">
+  <ul className="flex flex-col gap-2">
     {menuItems.map((menuItem) => {
       if ("items" in menuItem) {
-        return (
-          <li className="mt-4">
-            <span className="ml-2 inline-block pb-1">
-              <Text size="sm">
-                <strong>{menuItem.name}</strong>
-              </Text>
-            </span>
-            <ul className="flex flex-col gap-2">
-              {menuItem.items.map((menuItem) => (
-                <MenuItem {...menuItem} />
-              ))}
-            </ul>
-          </li>
-        )
+        return <Category {...menuItem} />
       } else {
         return <MenuItem {...menuItem} />
       }
     })}
   </ul>
+)
+
+const Category = ({ name, items }: MenuCategory) => (
+  <li className="mt-4">
+    <span className="inline-block px-2 py-4">
+      <Text size="sm">
+        <strong>{name}</strong>
+      </Text>
+    </span>
+    <ul className="flex flex-col gap-2">
+      {items.map((menuItem) => (
+        <MenuItem {...menuItem} />
+      ))}
+    </ul>
+  </li>
 )
 
 const MenuItem: React.FC<MenuItemProps & { active?: true }> = ({
@@ -34,23 +41,26 @@ const MenuItem: React.FC<MenuItemProps & { active?: true }> = ({
   icon,
 }) => {
   const isExternal = href.startsWith("http")
-  const classes = [
-    "absolute z-0 inset-1 group-hover:-inset-0 group-active:-inset-1 rounded-lg bg-zinc-200 dark:bg-zinc-800 opacity-0 group-hover:opacity-100 group-active:opacity-100 transition-all duration-200 ease-bounce",
-  ]
-
-  if (active) classes.push("opacity-100")
 
   return (
     <a href={href} rel="prefetch" target={isExternal ? "_blank" : "_self"}>
       <li className="group relative p-2 text-zinc-800 dark:text-zinc-50 dark:hover:text-white">
-        <span className={classes.join(" ")} />
+        <span
+          className={cn(
+            "absolute inset-1 z-0 ",
+            "ease-bounce transition-all duration-200",
+            "group-hover:-inset-0 group-hover:opacity-100 group-active:-inset-1 group-active:opacity-100",
+            "bg-zinc-200 dark:bg-zinc-800",
+            "rounded-lg",
+            active ? "opacity-100" : "opacity-0"
+          )}
+        />
         <span className="relative z-10">
           <Text as="span" size="sm" color="inherit">
             <span className="flex flex-row items-center gap-2">
-              <Icon name={icon} />
-              {name}
-              {isExternal && <Icon name="external-link" />}
-            </span>
+              <Icon name={icon} /> {name}{" "}
+              {isExternal && <Icon name="external-link" />}{" "}
+            </span>{" "}
           </Text>
         </span>
       </li>
